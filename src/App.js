70c813488e1f69eaf8748bd64b6a13909431f4c4
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-09-06 09:16:06 +0200

    components, containers all setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers';
+import {CTA, Brand, Navbar} from './components';
 
 const App = () => {
   return (
---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+import {default as Blog } from './blog/Blog';
+import {default as Features } from './features/Features';
+import {default as Footer } from './footer/Footer';
+import {default as Header } from './header/Header';
+import {default as Possibility } from './possibility/Possibility';
+import {default as whatGPT3 } from './whatGPT3/WhatGPT3';
