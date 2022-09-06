d57e6f9e68e7bbdaf2af39f2a0c3fcb71204d51e
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-09-06 15:36:20 +0200

    navbar + assets from Figma

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 import './navbar.css';
 
 
 const Navbar = () => {
   return (
-    <div >Navbar</div>
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-logo'>
+          <img src={ logo } alt="logo"/>
+        </div>
+
+      </div>
+      </div>
   )
 }
 
