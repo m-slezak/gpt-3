f92ca0cb30d45bd7312989e524e971eeb0e64d38
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-09-06 09:27:04 +0200

    structure setup

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import {CTA, Brand, Navbar} from './components';
 
 const App = () => {
   return (
-    <div>
-    <h1>
-        Siemanko
-    </h1>
+    <div className='App'>
+        <div className='gradient__bg'>
+           <Navbar />
+           <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
     </div>
   )
 }
