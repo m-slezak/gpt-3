1723d7272608292a85eba10b8fa01652a4249762
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-09-13 19:07:58 +0200

    Possibility complete

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 const App = () => {
   return (
     <div className='App'>
-        <div className='gradient__bg'>
+        <div className='gradient__bg '>
            <Navbar />
            <Header />
         </div>
