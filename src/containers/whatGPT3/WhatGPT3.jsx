b8267879a82ef1b0238d16e4115fd776af765ec3
m-slezak <77272905+m-slezak@users.noreply.github.com>
2022-09-13 10:50:17 +0200

    WHATGPT3, Feature - structure

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import Feature from '../../components/feature/Feature';
 import './whatGPT3.css';
 
 
 const WhatGPT3 = () => {
   return (
-    <div >WhatGPT3</div>
+    <div className="gpt3__whatgpt3 section__margin" >
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className='gradient__text'>
+        The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+      <Feature />
+      <Feature />
+      <Feature />
+      </div>
+    </div>
   )
 }
 
